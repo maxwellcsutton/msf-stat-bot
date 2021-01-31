53899d580518decb2bd92dc9d96e7f7579c679b2
maxwellcsutton <maxwellcsutton@gmail.com>
2021-01-30 21:12:07 -0800

    adding error handling so bot doesn't crash if things go wrong

--- discordBot.js
+++ discordBot.js
@@ -21,19 +21,26 @@ discord.on("message", async function(message) {
     const command = args.shift().toLowerCase()
 
     if (command === "war") {
-        // checks if the message has an attachment
-        let msgAttachments = message.attachments.array()[0]
-        if (!msgAttachments) {
-            message.reply("Please add an image to your request.")
-            return
+        try {
+            // checks if the message has an attachment
+            let msgAttachments = message.attachments.array()[0]
+            if (!msgAttachments) {
+                message.reply("Please add an image to your request.")
+                return
+            }
+            // upload the file then pass to war app
+            let upload = msgAttachments.url
+            message.reply(`I'm processing your request.  Please stand by.`)
+            let csv = await app.runWarApp(upload)
+            let timeTaken = Date.now() - message.createdTimestamp
+            message.reply(`I've processed your request in ${timeTaken/1000} seconds.  CSV output is attached.`)
+            const attachment = new MessageAttachment(csv)
+            message.reply(attachment)
+        } catch (err) {
+            if (err) {
+                message.reply("I was not able to process your request.  Please make sure an image of the proper format was uploaded.  Only portrait mode images from war results are accepted.")
+                return
+            }
         }
-        // upload the file then pass to war app
-        let upload = msgAttachments.url
-        message.reply(`I'm processing your request.  Please stand by.`)
-        let csv = await app.runWarApp(upload)
-        let timeTaken = Date.now() - message.createdTimestamp
-        message.reply(`I've processed your request in ${timeTaken/1000} seconds.  CSV output is attached.`)
-        const attachment = new MessageAttachment(csv)
-        message.reply(attachment)
     }
 })
