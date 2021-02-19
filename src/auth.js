84e2434b7e31fb63fc21fccb20394127ff8b0363
maxwellcsutton <57697520+maxwellcsutton@users.noreply.github.com>
2021-02-18 16:53:51 -0800

    adding ./config dir if it doesnt exist (#4)

--- src/auth.js
+++ src/auth.js
@@ -3,6 +3,10 @@ import dotenv from "dotenv"
 
 export default class {
     async setEnvVars() {
+        let dir = "../config"
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
         dotenv.config({ path: '../.env' })
         fs.writeFileSync("../config/mdtGoogleApiCreds.json", process.env.CREDS)
     }
